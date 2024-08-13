a20ace64bc74fa566ffee2b4c2422120626adcd1
João Victor F. Lima <50875924+jvfreitasl@users.noreply.github.com>
2024-08-13 16:39:35 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Alterando o arquivos apos ter feito o git pushgit </h1>
+function Home() {
+  return <h1>Alterando o arquivos apos ter feito o git pushgit </h1>;
 }
 
 export default Home;
